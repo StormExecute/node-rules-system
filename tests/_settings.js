4c6ba63542b768486784753e8ac807870f79ca22
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-30 23:41:47 +0300

    change: MAX_WAIT_INTERVAL -> MAX_WAIT_INTERVAL_BEFORE_THROW && add: MAX_WAIT_INTERVAL_BEFORE_NEXT_TEST

--- tests/_settings.js
+++ tests/_settings.js
@@ -1,8 +1,11 @@
 const options = {
 
 	NRS_PASSWORD: "123".repeat(3) + Math.random(),
+
 	TEST_SITE: "www.example.com",
-	MAX_WAIT_INTERVAL: 5500,
+
+	MAX_WAIT_INTERVAL_BEFORE_THROW: 5500,
+	MAX_WAIT_INTERVAL_BEFORE_NEXT_TEST: 700,
 
 };
 
