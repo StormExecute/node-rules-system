648f504420eb643481d7299b125a5fbfe0262f67
Ghost <vladimirvsevolodovi@yandex.ru>
2020-10-31 22:14:05 +0300

    add: tests/_settings: message about the risk of password export

--- tests/_settings.js
+++ tests/_settings.js
@@ -1,6 +1,10 @@
 const options = {
 
+	//this is a test export, DO NOT USE THIS in real projects, as it will make it easier for malicious modules to hack the application!
 	NRS_PASSWORD: "123".repeat(3) + Math.random(),
+	//for real projects DO NOT EXPORT the password in any case and use the following construction once in main file:
+	//const pass = "your_pass" + Math.random();
+	//NRS.init(pass);
 
 	TEST_SITE: "www.example.com",
 
