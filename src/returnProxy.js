5bd1f7022216095f51a97fa0d588ec829b978c75
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-03 22:13:18 +0300

    add: await support

--- src/returnProxy.js
+++ src/returnProxy.js
@@ -1,18 +1,30 @@
+const get = (_, prop) => {
+
+	if(prop == Symbol.toPrimitive) {
+
+		return () => "0"
+
+	} else if(prop == "then") {
+
+		return awaitProxy;
+
+	} else {
+
+		return returnProxy;
+
+	}
+
+}
+
+const awaitProxy = new Proxy({}, {
+
+	get,
+
+})
+
 const returnProxy = new Proxy(class {}, {
 
-	get: (_, prop) => {
-
-		if(prop == Symbol.toPrimitive) {
-
-			return () => "0"
-
-		} else {
-
-			return returnProxy;
-
-		}
-
-	},
+	get,
 	has: () => returnProxy,
 	deleteProperty: () => returnProxy,
 	apply: () => returnProxy,
