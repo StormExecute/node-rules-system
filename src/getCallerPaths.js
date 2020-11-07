17a378b8e6a0ef65de34d72e32eafbee79ecb180
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-07 21:07:26 +0300

    add: new paths to skip to reveal the real nativePath

--- src/getCallerPaths.js
+++ src/getCallerPaths.js
@@ -43,21 +43,58 @@ function main(callerPaths, errStack) {
 			inProcessScreening
 			&&
 			(
+
 				endsWithTranslationSlashes(path, "/node-rules-system/src/getCallerPaths.js")
 				||
 				endsWithTranslationSlashes(path, "/node-rules-system/src/secureSession.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/logs.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/password.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/_settings/main.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/getFunctionality.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/whiteListFunctionality.js")
+
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/cluster/allow.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/worker_threads/allow.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/dgram/allow.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/child_process/allow.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/process/allowBindings.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/connections/allow.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/fs/allow.js")
+
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/cluster/block.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/worker_threads/block.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/dgram/block.js")
+				||
+				endsWithTranslationSlashes(path, "/node-rules-system/src/child_process/block.js")
 				||
 				endsWithTranslationSlashes(path, "/node-rules-system/src/process/blockBindings.js")
 				||
 				endsWithTranslationSlashes(path, "/node-rules-system/src/connections/block.js")
 				||
 				endsWithTranslationSlashes(path, "/node-rules-system/src/fs/block.js")
+
 				||
 				endsWithTranslationSlashes(path, "/node-rules-system/src/integrateFunctionality/toFns.js")
 				||
 				endsWithTranslationSlashes(path, "/node-rules-system/src/integrateFunctionality/toObject.js")
 				||
 				endsWithTranslationSlashes(path, "/node-rules-system/src/integrateFunctionality/toProtoFn.js")
+
 			)
 		) {
 
