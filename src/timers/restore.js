877bc188559563c17932e311a363607a2809a68c
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-16 23:59:03 +0300

    up: src/timers/restore: based on new features

--- src/timers/restore.js
+++ src/timers/restore.js
@@ -1,6 +1,8 @@
 const { password, needToSetPassword, wrongPass } = require("../password");
 const { wrongPassEmitter } = require("../logs");
 
+const events = require('events');
+
 const $thisStore = require("./thisStore");
 
 const restore = require("../restore");
@@ -10,12 +12,28 @@ const thisRestoreFunctionality = {
 	setImmediate(tryPass){},
 	nextTick(tryPass){},
 
+	setTimeout(tryPass){},
+	setInterval(tryPass){},
+
+	then(tryPass){},
+	catch(tryPass){},
+
+	on(tryPass){},
+
 };
 
 [
 
 	[global, "setImmediate"],
 	[process, "nextTick"],
+
+	[global, "setTimeout"],
+	[global, "setInterval"],
+
+	[global.Promise.prototype, "then"],
+	[global.Promise.prototype, "catch"],
+
+	[events.EventEmitter.prototype, "on"],
 
 ].forEach( ( [el, prop] ) => {
 
@@ -27,10 +45,26 @@ const thisRestoreFunctionality = {
 		if (prop == "setImmediate" && $thisStore.statusImmediate == false) return false;
 		if (prop == "nextTick" && $thisStore.statusNextTick == false) return false;
 
+		if (prop == "setTimeout" && $thisStore.statusSetTimeout == false) return false;
+		if (prop == "setInterval" && $thisStore.statusSetInterval == false) return false;
+
+		if (prop == "then" && $thisStore.statusPromiseThen == false) return false;
+		if (prop == "catch" && $thisStore.statusPromiseCatch == false) return false;
+
+		if (prop == "on" && $thisStore.statusEventEmitterOn == false) return false;
+
 		restore([prop], el, $thisStore);
 
 		if (prop == "setImmediate" && $thisStore.statusImmediate == true) $thisStore.statusImmediate = false;
 		if (prop == "nextTick" && $thisStore.statusNextTick == true) $thisStore.statusNextTick = false;
+
+		if (prop == "setTimeout" && $thisStore.statusSetTimeout == true) $thisStore.statusSetTimeout = false;
+		if (prop == "setInterval" && $thisStore.statusSetInterval == true) $thisStore.statusSetInterval = false;
+
+		if (prop == "then" && $thisStore.statusPromiseThen == true) $thisStore.statusPromiseThen = false;
+		if (prop == "catch" && $thisStore.statusPromiseCatch == true) $thisStore.statusPromiseCatch = false;
+
+		if (prop == "on" && $thisStore.statusEventEmitterOn == true) $thisStore.statusEventEmitterOn = false;
 
 		return true;
 
@@ -47,6 +81,14 @@ thisRestoreFunctionality.all = function (tryPass) {
 
 		thisRestoreFunctionality.setImmediate(tryPass),
 		thisRestoreFunctionality.nextTick(tryPass),
+
+		thisRestoreFunctionality.setTimeout(tryPass),
+		thisRestoreFunctionality.setInterval(tryPass),
+
+		thisRestoreFunctionality.then(tryPass),
+		thisRestoreFunctionality.catch(tryPass),
+
+		thisRestoreFunctionality.on(tryPass),
 
 	];
 
