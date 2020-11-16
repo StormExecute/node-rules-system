ab3f5e8e296037fbf5ab8f273f2c300ab60ccc01
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-16 23:49:55 +0300

    up: src/timers/thisStore: based on new features

--- src/timers/thisStore.js
+++ src/timers/thisStore.js
@@ -3,7 +3,23 @@ module.exports = {
 	statusImmediate: false,
 	statusNextTick: false,
 
+	statusSetTimeout: false,
+	statusSetInterval: false,
+
+	statusPromiseThen: false,
+	statusPromiseCatch: false,
+
+	statusEventEmitterOn: false,
+
 	setImmediate: null,
 	nextTick: null,
+
+	setTimeout: null,
+	setInterval: null,
+
+	then: null,
+	"catch": null,
+
+	on: null,
 
 };
