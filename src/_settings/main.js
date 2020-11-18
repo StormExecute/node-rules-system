7fa9c2f5f8ac7aa661ae71b0efb5f6adb931d31c
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-18 21:31:30 +0300

    add: src/_settings/main: setCorePath functionality

--- src/_settings/main.js
+++ src/_settings/main.js
@@ -4,6 +4,8 @@ const { wrongPassEmitter } = require("../logs");
 
 const storeSettings = require("./store");
 
+const { $corePath } = require("../whiteListFunctionality");
+
 module.exports = {
 
 	throwIfWrongPassword(tryPass) {
@@ -23,6 +25,19 @@ module.exports = {
 		if (tryPass != password.value) return wrongPassEmitter(wrongPass, "dontThrowIfWrongPassword");
 
 		storeSettings.throwIfWrongPassword = false;
+
+		return true;
+
+	},
+
+	setCorePath(tryPass, path) {
+
+		if (password.value === null) throw new Error(needToSetPassword);
+		if (tryPass != password.value) return wrongPassEmitter(wrongPass, "setCorePath", { path });
+
+		if(typeof path != "string") return false;
+
+		$corePath.value = path;
 
 		return true;
 
