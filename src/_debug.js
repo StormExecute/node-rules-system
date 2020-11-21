46839257327c9b4b3517d2d11ecf67e804ab7598
Ghost <vladimirvsevolodovi@yandex.ru>
2020-11-21 23:47:48 +0300

    git: add src/_debug core

---
+++ src/_debug.js
@@ -0,0 +1,83 @@
+const debugMethods = ["integrate", "getCallerPaths", "fileNames", "timers"];
+const debugShortMethods = ["debugI", "debugP", "debugF", "debugT"];
+
+class debugC {
+
+	constructor() {
+
+		this.consoleLog = console.log;
+
+		if(this.haveArg("debugAll")) {
+
+			for (let i = 0; i < debugMethods.length; ++i) {
+
+				this[ "$" + debugMethods[i] ] = true;
+
+			}
+
+		} else {
+
+			for (let i = 0; i < debugMethods.length; ++i) {
+
+				this[ "$" + debugMethods[i] ] = this.haveArg( debugShortMethods[i] );
+
+			}
+
+		}
+
+	}
+
+	haveArg(arg) {
+
+		return !!process.argv.filter(el => el == "--nrs-" + arg).length || process.env[arg];
+
+	}
+
+	log() {
+
+		this.consoleLog(...arguments);
+
+		return true;
+
+	}
+
+	_log(method, ...args) {
+
+		if(this["inProcessLog" + method]) return false;
+
+		this["inProcessLog" + method] = true;
+
+		this.consoleLog(...args);
+
+		this["inProcessLog" + method] = false;
+
+		return true;
+
+	}
+
+	integrate(){}
+	getCallerPaths(){}
+	fileNames(){}
+	timers(){}
+
+}
+
+for (let i = 0; i < debugMethods.length; ++i) {
+
+	debugC.prototype[debugMethods[i]] = function () {
+
+		if( this[ "$" + debugMethods[i] ] ) {
+
+			return this._log(debugMethods[i], ...arguments);
+
+		}
+
+		return false;
+
+	}
+
+}
+
+const debug = new debugC;
+
+module.exports = debug;
