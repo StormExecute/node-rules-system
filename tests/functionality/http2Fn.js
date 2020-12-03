b561a133ed1d802a67fd4ee4aa92d2b249730921
Ghost <vladimirvsevolodovi@yandex.ru>
2020-12-03 21:09:05 +0300

    fix: tests/functionality/http2Fn: for previous node versions

--- tests/functionality/http2Fn.js
+++ tests/functionality/http2Fn.js
@@ -9,7 +9,11 @@ module.exports = function fn(should, client) {
 		.on("data", chunk => {})
 		.on("end", () => {
 
-			client.close();
+			if(typeof client.close == "function") {
+
+				client.close();
+
+			}
 
 			if(should == "allow") return process.thenTest(true);
 			else return process.thenTest("must be blocked!")
