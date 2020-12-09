a486cca198527b621190bb52eddd249ed554fd72
Ghost <vladimirvsevolodovi@yandex.ru>
2020-12-09 23:41:54 +0300

    fix: tests/functionality/netCallback: double call prevention

--- tests/functionality/netCallback.js
+++ tests/functionality/netCallback.js
@@ -1,6 +1,12 @@
+let then = false;
+
 module.exports = function make(should) {
 
 	return function (data) {
+
+		if(then) return;
+
+		then = true;
 
 		if(should == "allow") return process.thenTest(true);
 		else return process.thenTest("must be blocked!")
