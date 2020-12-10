3bbdf3363333c8756f0342ac3f3210a67d615601
Ghost <vladimirvsevolodovi@yandex.ru>
2020-12-10 23:47:03 +0300

    add: src/_data/index: parseRandomInCodeMatch && export

--- src/_data/index.js
+++ src/_data/index.js
@@ -1,9 +1,13 @@
 const prefix = "[node-rules-system]";
 const prefixS = prefix + " ";
 
+const parseRandomInCodeMatch = "[[NRS_RANDOM_FN_SALT]]";
+
 module.exports = {
 
 	prefix,
 	prefixS,
+
+	parseRandomInCodeMatch,
 
 }
