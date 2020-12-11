83a47d848192eed3995eee1ddaaa3b0e36258d48
Ghost <vladimirvsevolodovi@yandex.ru>
2020-12-11 23:36:22 +0300

    git: add src/_data/objectP core

---
+++ src/_data/objectP.js
@@ -0,0 +1,9 @@
+const ObjectDefineProperty = Object.defineProperty;
+const ObjectDefineProperties = Object.defineProperties;
+
+module.exports = {
+
+	ObjectDefineProperty,
+	ObjectDefineProperties,
+
+};
