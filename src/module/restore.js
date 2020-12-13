635dbec6201df8fd7452d0b3eb038b3a8a3bd377
Ghost <vladimirvsevolodovi@yandex.ru>
2020-12-13 23:05:38 +0300

    up:src/module/restore:offSecureRequirePatch,offDependencyController

--- src/module/restore.js
+++ src/module/restore.js
@@ -1,28 +1,139 @@
 const { password, needToSetPassword, wrongPass } = require("../password");
 
+const settings = require("../_settings/store");
+
 const { wrongPassEmitter } = require("../logs");
 
 const Module = require('module');
-
 const $Module = require("./store");
 
 const restore = require("../restore");
+
+const wrapStore = require("./wrapStore");
+const { randomSignChanger } = require("./wrap");
+
+function offSecureRequirePatch(tryPass) {
+
+	if(password.value === null) throw new Error(needToSetPassword);
+	if(tryPass != password.value) return wrongPassEmitter(wrongPass, "offSecureRequirePatch");
+
+	if($Module.statusSecureRequire == false) return [false, $Module.statusPatchWrap];
+
+	$Module.secureRequire = null;
+	$Module.secureRequireArgsLink = null;
+
+	if(!$Module.statusDependencyController) {
+
+		restore(["wrap"], Module, $Module);
+
+		delete global[wrapStore.utils];
+
+		if(settings.changeModuleRandomSignInterval && randomSignChanger.timeout) {
+
+			clearTimeout(randomSignChanger.timeout);
+
+		}
+
+		$Module.statusPatchWrap = false;
+
+	}
+
+	$Module.statusSecureRequire = false;
+
+	return [true, $Module.statusPatchWrap];
+
+}
+
+function offDependencyController(tryPass) {
+
+	if(password.value === null) throw new Error(needToSetPassword);
+	if(tryPass != password.value) return wrongPassEmitter(wrongPass, "offDependencyController");
+
+	if($Module.statusDependencyController == false) return [false, $Module.statusPatchWrap];
+
+	delete global[wrapStore.dRules];
+
+	$Module.dependencyController = null;
+
+	if(!$Module.statusSecureRequire) {
+
+		restore(["wrap"], Module, $Module);
+
+		delete global[wrapStore.utils];
+
+		if(settings.changeModuleRandomSignInterval && randomSignChanger.timeout) {
+
+			clearTimeout(randomSignChanger.timeout);
+
+		}
+
+		$Module.statusPatchWrap = false;
+
+	}
+
+	$Module.statusDependencyController = false;
+
+	return [true, $Module.statusPatchWrap];
+
+}
 
 function restoreOriginalRequire(tryPass) {
 
 	if(password.value === null) throw new Error(needToSetPassword);
 	if(tryPass != password.value) return wrongPassEmitter(wrongPass, "restoreOriginalRequire");
 
-	if($Module.status == false) return false;
+	//secureRequire, dependencyController, moduleWrap
+	const result = [false, false, false];
 
-	delete global[$Module.secureRequireSecretEmitter];
+	if($Module.statusSecureRequire == true) {
 
-	restore(["wrap", "secureRequireSecretEmitter"], Module, $Module);
+		$Module.secureRequire = null;
+		$Module.secureRequireArgsLink = null;
 
-	$Module.status = false;
+		$Module.statusSecureRequire = false;
 
-	return true;
+		result[0] = true;
+
+	}
+
+	if($Module.statusDependencyController == true) {
+
+		delete global[wrapStore.dRules];
+		$Module.dependencyController = null;
+
+		$Module.statusDependencyController = false;
+
+		result[1] = true;
+
+	}
+
+	if($Module.statusPatchWrap == true) {
+
+		restore(["wrap"], Module, $Module);
+
+		delete global[wrapStore.utils];
+
+		if(settings.changeModuleRandomSignInterval && randomSignChanger.timeout) {
+
+			clearTimeout(randomSignChanger.timeout);
+
+		}
+
+		$Module.statusPatchWrap = false;
+
+		result[2] = true;
+
+	}
+
+	return result;
 
 }
 
-module.exports = restoreOriginalRequire;
+module.exports = {
+
+	offSecureRequirePatch,
+	offDependencyController,
+
+	restoreOriginalRequire,
+
+};
