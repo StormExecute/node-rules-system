a637bcf642042a6bf001061d2527e0ddf7dfa1df
Ghost <vladimirvsevolodovi@yandex.ru>
2020-12-14 23:39:18 +0300

    del: src/_data/objectP && src/_data/random: no need

--- src/_data/objectP.js
+++
@@ -1,9 +0,0 @@
-const ObjectDefineProperty = Object.defineProperty;
-const ObjectDefineProperties = Object.defineProperties;
-
-module.exports = {
-
-	ObjectDefineProperty,
-	ObjectDefineProperties,
-
-};
