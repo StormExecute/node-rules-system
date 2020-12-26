6b89cd411101fa48f523275ff6a09409e2118f34
Ghost <vladimirvsevolodovi@yandex.ru>
2020-12-26 21:44:33 +0300

    up: webpack.config: final freeze processing for lib/NRS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const webpack = require("webpack");
+
 const TerserPlugin = require("terser-webpack-plugin");
 
 const nodePath = require("path");
+const fs = require("fs");
 
 const webpackOptions = {
+
+	mode: "production",
 
 	entry: "./src",
 
@@ -56,4 +61,47 @@ const webpackOptions = {
 
 };
 
-module.exports = webpackOptions;
+if( process.argv[1].endsWith( "webpack-cli" ) ) {
+
+	module.exports = webpackOptions;
+
+} else {
+
+	webpack(webpackOptions, (err, stats) => {
+
+		if (!err) err = stats.toJson().errors[0];
+
+		if (err) {
+
+			throw new Error(err);
+
+		}
+
+		console.log(stats.toString({colors: true}));
+
+		const content = fs.readFileSync( nodePath.join(process.cwd(), "lib/NRS.js") ).toString();
+
+		const NRS_PRIMORDIALS = `let a=require("http")["NRS_PRIMORDIALS"];`;
+		const FREEZE = `a.ObjectFreeze(e[n]);a.ObjectDefineProperty(t,n,{enumerable:!0,value:e[n]})`;
+
+		const finalContent = content.replace(
+			/!function\(t,e\){for\(var n in e\)t\[n\]=e\[n\];/,
+			`!function(t,e){${NRS_PRIMORDIALS}for(var n in e){${FREEZE}}`
+		);
+
+		fs.writeFile( nodePath.join(process.cwd(), "lib/NRS.js"), finalContent, err => {
+
+			if(err) {
+
+				throw new Error(err);
+
+			}
+
+			console.log("DONE!");
+			process.exit(0);
+
+		});
+
+	});
+
+}
